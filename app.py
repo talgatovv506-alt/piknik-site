a5459b09c78b6edfba4bb92921236ccda47105ae
talga <talgatovv506@gmail.com>
2026-06-03 00:43:40 +0500

    Кәусарға шақырту

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Сәлеметсіз бе! Бұл менің Flask сайтым 🚀'
+def home():
+    return render_template('index.html')
+
+@app.route('/rakmet')
+def rakmet():
+    return render_template('rakmet.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
